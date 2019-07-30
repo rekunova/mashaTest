95c09e7d1cd81dce985822c4d023816816608953
v_shliahtur <shliahtur@gmail.com>
2019-07-30 15:05:59 +0300

    й

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -34,7 +34,8 @@ function MainContent() {
             />
 
             <Joke
-                punchline="I am tired as hell"
+             question =" have you seen me?"
+
             />
 
         </div>
